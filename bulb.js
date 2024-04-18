6010465f5a575d88e6f3ed55db84157de15db5ed
ALLAM NEHAL VARMA <157111867+NehalVarma@users.noreply.github.com>
2024-04-18 14:38:23 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,13 @@
+
+if(onclick==myFunction){
+
+function myFunction(){
+    document.getElementById("myImage").src="on.png";     
+}
+
+}
+if(onclick==myFunction2){
+function myFunction2(){
+    document.getElementById("myImage").src="off.png"; 
+}
+}
